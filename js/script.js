ad2159362d065fa14e76d79ef7ab0fe4c6bcfd54
Andrea Bottini <andrea.bottini22@gmail.com>
2019-12-18 16:20:38 +0100

    Impostazione esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+// alert('funziona la pagina?')
+
+// Quanti anni ha l'utente?
+//
+// Quanti chilometri deve percorrere l'utente per viaggiare?
+//
+// è necessario definire il costo del biglietto in base ai chilometri che l'utente percorre
+//
+// Se l'utente è minorenne andrà applicato uno sconto del 20%
+//
+// Se l'utente è over 65 andrà applicato uno sconto del 40%
+//
+// Se gli utenti hanno una età compresa tra i 18 anni e i 64 anni andrà applicata la tariffa normale
